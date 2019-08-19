bdf8ab317e6d364f67c134fcd02f2c0facd8ebf9
Adam Samulski <a.samulski@op.pl>
2019-08-19 21:59:16 +0200

    simple structure html & addEL

--- RockPaperScissors/game.js
+++ RockPaperScissors/game.js
@@ -1,2 +1,23 @@
-console.log("działa")
-prompt("aksdks");
+const gameResult = {
+    numbers: 0,
+    wins: 0,
+    losses: 0,
+    draws: 0
+
+}
+const game = {
+    playerChoice: "",
+    computerChoice: "",
+    playerChoiceHTML: ""
+}
+const selections = [...document.querySelectorAll('.select img')];
+
+
+function playerSelect() {
+    game.playerChoice = this.dataset.option
+    console.log(game.playerChoice);
+    this.style.boxShadow = '0 0 0 4px brown'
+
+}
+
+selections.forEach(select => select.addEventListener('click', playerSelect))

f43e080e61403a3a06bbdb9b2b8c79224bbd8ce3
Adam Samulski <a.samulski@op.pl>
2019-08-20 21:06:49 +0200

    start game function

--- RockPaperScissors/game.js
+++ RockPaperScissors/game.js
@@ -15,10 +15,22 @@ const selections = [...document.querySelectorAll('.select img')];
 
 function playerSelect() {
     game.playerChoice = this.dataset.option
-    console.log(game.playerChoice);
+    // console.log(game.playerChoice);
     selections.forEach(select => select.style.boxShadow = '');
     this.style.boxShadow = '0 0 2px 4px brown';
 
 }
+function randomization() {
+    return selections[Math.floor(Math.random() * 3)].dataset.option;
+
+}
+function startGame() {
+    if (!game.playerChoice) return alert('Wybierz: Kamień, Papier lub Nożyce')
+    game.computerChoice = randomization()
+    // console.log(game.computerChoice);
+
+}
 
 selections.forEach(select => select.addEventListener('click', playerSelect))
+
+document.querySelector('.start').addEventListener('click', startGame);

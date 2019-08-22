761dc26918619b70dfc57849a204f91c9657490b
Adam Samulski <a.samulski@op.pl>
2019-08-22 20:19:43 +0200

    clock interval

--- clock/clock.js
+++ clock/clock.js
@@ -1,1 +1,10 @@
-console.log("ksjjkajs");
+const setTime = () => {
+    const time = new Date();
+
+    const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+    const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+    const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+    document.querySelector('.watch span').textContent = `${hours}:${minutes}:${seconds}`
+}
+setInterval(setTime, 1000);
+
